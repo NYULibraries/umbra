47a9ec6a01688d560c00f6ac04cad95604b89a86
B Alter <barnaby.alter@gmail.com>
2013-07-01 16:45:08 -0400

    Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set(:branch, 'master') unless exists?(:branch)
 set :git_enable_submodules, 1
 
 # Environments
-set :stages, ["staging", "production"]
+set :stages, %w{staging production}
 set :default_stage, "staging"
 set :keep_releases, 5
 set :use_sudo, false
