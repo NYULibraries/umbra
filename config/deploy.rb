c78e3452bb1ab4aa6522c7136ec0a092c76b80ea
B Alter <barnaby.alter@gmail.com>
2013-07-01 16:56:13 -0400

    Clear a cache

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,18 +24,17 @@ set :bundle_without, [:development, :test]
 
 # Git vars
 set :repository, "git@github.com:NYULibraries/umbra.git" 
-set :scm, :git
+set :scm, :gitg
 set :deploy_via, :remote_cache
 set(:branch, 'master') unless exists?(:branch)
 set :git_enable_submodules, 1
-
 
 set :keep_releases, 5
 set :use_sudo, false
 
 # Configure app_settings from rails_config
 # Defer processing until we have rails environment
-set(:app_settings) { eval(run_locally("rails runner -e #{rails_env} 'p Settings.capistrano.to_hash'")) }
+set(:app_settings) { run_locally("rails runner -e #{rails_env} 'p Settings.capistrano.to_hash'") }
 set(:scm_username) { app_settings[:scm_username] }
 set(:app_path) { app_settings[:path] }
 set(:user) { app_settings[:user] }
