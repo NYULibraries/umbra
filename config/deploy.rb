fb525157d6fc7a4491ced9cbe646812465736e65
hab278 <as>
2013-07-11 18:05:37 -0400

    This is dumb simple now

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,4 @@
 require 'capistrano-nyu'
 
 set :app_title, "umbra"
-set :application, "#{app_title}_repos"
-
-# Git vars
 set :repository, "git@github.com:NYULibraries/umbra.git"
-
-set :recipient, "web.services@library.nyu.edu"
