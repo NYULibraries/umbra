c6ab25d1bd372a0de69a8e870e9cbe494f6c7741
hab278 <as>
2013-07-11 16:10:29 -0400

    I think it works :elephant:

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,10 +7,10 @@ set :application, "#{app_title}_repos"
 # RVM  vars
 set :rvm_ruby_string, "1.9.3-p448"
 
-
 # Git vars
 set :repository, "git@github.com:NYULibraries/umbra.git" 
 
-
 # Rails specific vars
 set :normalize_asset_timestamps, false
+
+set :recipient, "web.services@library.nyu.edu"
