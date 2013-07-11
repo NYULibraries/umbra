ff255dd582e9492583cc08246b4674a105786d9d
hab278 <as>
2013-07-11 16:31:52 -0400

    Even slimmer deploy???? whaaaaat

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,7 @@
 require 'capistrano-nyu'
 
-set :ssh_options, {:forward_agent => true}
 set :app_title, "umbra"
 set :application, "#{app_title}_repos"
 
-# RVM  vars
-set :rvm_ruby_string, "1.9.3-p448"
-
 # Git vars
 set :repository, "git@github.com:NYULibraries/umbra.git"
-set :branch, "master"
-
-# Rails specific vars
-set :normalize_asset_timestamps, false
