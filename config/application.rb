70a9bcc8d1db1b0f27c96e74d7dd19bc4995d8cc
B Alter <barnaby.alter@gmail.com>
2014-05-22 16:06:39 -0400

    Downgraded assets gem

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ if defined?(Bundler)
 end
 
 require 'nyulibraries-assets'
- 
+
 require 'figs'
 # Don't run this initializer on travis.
 Figs.load(stage: Rails.env) unless ENV['TRAVIS']
@@ -59,7 +59,7 @@ module Umbra
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true    
+    config.assets.enabled = true
     # Default SASS Configuration, check out https://github.com/rails/sass-rails for details
     config.assets.compress = !Rails.env.development?
 
