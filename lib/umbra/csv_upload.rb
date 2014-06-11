6e2b41b66bd7aa3b65e65d8de5b1e6848625fdfe
B Alter <barnaby.alter@gmail.com>
2014-06-11 16:56:06 -0400

    Fixed bulk upload for new version of acts_as_taggle_on gem which doesnt allow duplicate facets and uncovered mistake in uploading facets

--- lib/umbra/csv_upload.rb
+++ lib/umbra/csv_upload.rb
@@ -1,12 +1,14 @@
 ##
 ## Class for handling batch uploads from CSV
 ##
+## TODO: This is way too confusing method of adding facets from CSV; Document!
+##
 require 'csv'
 module Umbra
   class CsvUpload
     include Umbra::Collections
     attr_accessor :csv_file, :current_user
-    
+
     def initialize(csv_file, current_user)
       @csv_file = csv_file
       @current_user = current_user
@@ -17,17 +19,17 @@ module Umbra
       # Did the user submit a file?
       if csv_file.present?
         # If this is a valid CSV file, proceed to uploading
-        if is_valid_csv? 
+        if is_valid_csv?
           csv_to_db
           return "Successfully loaded CSV records into database. Reindexing will take a moment."
-        else 
+        else
           return "File must be in CSV format."
         end
       else
         return "Please select a file to upload."
-      end      
+      end
     end
-      
+
     # Pull fields out of the CSV into db
     def csv_to_db(encoding = "windows-1251:utf-8")
       Rails.logger.info "Starting job for #{csv_file}"
@@ -40,22 +42,24 @@ module Umbra
       write_pid_file(pid_failed)
     end
     private :csv_to_db
-    
+
     def csv_row_to_db(row)
       # User can only update records he has access to
       if current_user_has_access_to_collection? row["nyu.libraries.collection"]
-      
+
         #For each field in row, add facet to appropriate array
         row.each {|field| csv_field_to_db(field) }
-      
-        #Map each facet array to acts_as_taggable list 
-        facets.keys.each {|facet| facets_insert.merge!({facet.to_sym => facets[facet.to_sym]}) }
-        
-        csv_row_save(row)
+
+        #Map each facet array to acts_as_taggable list
+        facets_insert = Hash.new
+        facets.keys.each {|facet| facets_insert.merge!({facet.to_sym => facets[facet.to_sym] }) }
+
+        csv_row_save(row, facets_insert)
+        @facets = nil
       end
     end
-    
-    def csv_row_save(row)
+
+    def csv_row_save(row, facets_insert)
       #Generate record, or find if CSV contained a unique ID matching to original-id
       record = Umbra::Record.find_or_create_by_original_id(row["guid"])
       # Update attrs in the record and merge in facets
@@ -67,37 +71,45 @@ module Umbra
         :description => row[dc_format(:description)],
       }.merge(facets_insert))
     end
-    
+
     def csv_field_to_db(field)
       unless field[1].nil?
         facets.keys.each do |facet|
-          facets[facet.to_sym].push(field[1]) if field[0].eql? dc_format(facet)
+          # Complex little line to avoid pushing duplicates of the same tag
+          # definitely an ugly solution and going away soon
+          if facet_is_current_field(facet, field) && field_is_not_in_facet(field, facet)
+            facets[facet.to_sym].push(field[1])
+          end
         end
       end
     end
-    
+
+    def facet_is_current_field(facet, field)
+      field[0].eql? dc_format(facet)
+    end
+
+    def field_is_not_in_facet(field, facet)
+      !facets[facet.to_sym].map{|f| f.downcase}.include?(field[1].downcase)
+    end
+
     def facets
-      @facets ||= { :extent_list => [], :subject_controlled_list => [], :subject_tag_list => [], :coverage_spatial_list => [], :coverage_temporal_list => [], 
+      @facets ||= { :extent_list => [], :subject_controlled_list => [], :subject_tag_list => [], :coverage_spatial_list => [], :coverage_temporal_list => [],
                  :coverage_jurisdiction_list => [], :source_list => [], :language_list => [], :accrualPeriodicity_list => [] }
     end
-    
-    def facets_insert
-      @facets_insert ||= {}
-    end
-    
+
     # Get proper dublin core format from databse friendly version (i.e. subject_tag_list => dc.subject.tag)
     def dc_format(facet)
       facet.to_s.split("_list").first.split("_").unshift("dc").join(".")
     end
     private :dc_format
-    
+
     # Verify that the CSV is actually a CSV
     def is_valid_csv
       (csv_file.content_type == "text/csv")
     end
     alias_method :is_valid_csv?, :is_valid_csv
     private :is_valid_csv
-    
+
     # Create and write to the given PID file and folder structure
     def write_pid_file(pid_file)
       # Make temp/pids path if doesn't exist
@@ -106,23 +118,23 @@ module Umbra
       File.open(pid_file, 'w'){|f| f.write Process.pid}
     end
     private :write_pid_file
-    
+
     # Delete the passed in PID file
     def delete_pid_file(pid_file)
       File.delete pid_file if File.exists? pid_file
     end
     private :delete_pid_file
-    
+
     # Setup PID namespaced files so we know what's currently running
     def pid_namespace
       @pid_namespace ||= csv_file.original_filename.split(".")[0]
     end
     private :pid_namespace
-    
+
     def pid_failed
       @pid_failed ||= "#{Rails.root}/tmp/pids/#{pid_namespace}_csv_failed.pid"
     end
     private :pid_failed
-    
+
   end
 end
