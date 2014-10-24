572bee0f9c83d53a908171e4d6ecb47fa22668e9
B Alter <barnaby.alter@gmail.com>
2014-10-24 16:25:20 -0400

    Using acts_as_indexed for searching users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,22 @@ class UsersController < ApplicationController
   respond_to :html, :json, :csv
   respond_to :js, :only => [:update]
   # Privileged controller
-  before_filter :authenticate_admin
+  before_action :authenticate_admin
   # Edit the submitted admin collections based on existing collections in @user's db entry
-  before_filter :update_admin_collections, :only => [:update]
-  
+  before_action :update_admin_collections, :only => [:update]
+
   # GET /users
   def index
-    @users = User.search(params[:q]).order(sort_column + " " + sort_direction).page(params[:page]).per(30)
-    
+    @users = User.order(sort_column + " " + sort_direction).page(params[:page]).per(30)
+    @users = @users.with_query(params[:q]) unless params[:q].blank?
+
     respond_with(@users)
   end
 
   # GET /users/1
   def show
     @user = User.find(params[:id])
-    
+
     respond_with(@user)
   end
 
@@ -28,12 +29,12 @@ class UsersController < ApplicationController
       :umbra_admin => !@user.user_collections.empty?
     }
     @user.user_attributes = user_attributes
-    
-    flash[:notice] = t('users.update_success') if @user.save 
+
+    flash[:notice] = t('users.update_success') if @user.save
 
     respond_with(@user)
   end
-  
+
   # DELETE /users/1
   def destroy
     @user = User.find(params[:id])
@@ -41,18 +42,18 @@ class UsersController < ApplicationController
 
     respond_with(@user)
   end
-  
+
   # DELETE /users/clear_patron_data
   def clear_patron_data
     @users = User.all
-    
+
     flash[:success] = t('users.clear_patron_data_success') if User.destroy_all("user_attributes not like '%:umbra_admin: true%'")
-        
+
     respond_with(@users, :location => users_url) do |format|
       format.html { redirect_to users_url }
     end
   end
-  
+
   # Implement sort column for User class
   def sort_column
     super "User", "lastname"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,19 @@
 class User < ActiveRecord::Base
-  # Connects this user object to Blacklights Bookmarks and Folders. 
+  # Connects this user object to Blacklights Bookmarks and Folders.
   include Blacklight::User
   attr_accessible :crypted_password, :current_login_at, :current_login_ip, :email, :firstname, :last_login_at, :last_login_ip, :last_request_at, :lastname, :login_count, :mobile_phone, :password_salt, :persistence_token, :refreshed_at, :session_id, :user_attributes, :username
 
-  serialize :user_attributes  
+  serialize :user_attributes
 
   acts_as_authentic do |c|
     c.validations_scope = :username
     c.validate_password_field = false
-    c.require_password_confirmation = false  
+    c.require_password_confirmation = false
     c.disable_perishable_token_maintenance = true
   end
-  
-  # This search logic function protected against SQL injection
-  def self.search(search)
-    if search
-      q = "%#{search}%"
-      where('firstname LIKE ? || lastname LIKE ? || username LIKE ? || email LIKE ?', q, q, q, q)
-    else
-      # scoped to send back an ActiveRelation
-      scoped
-    end
-  end
-  
+
+  acts_as_indexed fields: [:firstname, :lastname, :username, :email]
+
   # Create a CSV format
   comma do
     username
@@ -30,22 +21,22 @@ class User < ActiveRecord::Base
     lastname
     email
   end
-  
+
   def has_admin_collections?
     (self.try(:user_attributes)[admin_collections_name].present?) rescue false
   end
-  
+
   def has_global_collection?
     self.try(:user_attributes)[admin_collections_name].include? "global" rescue false
   end
-  
+
   # Get user collections for @user instance, cast as Array if necessary
   def user_collections
-    @user_collections ||= (self.user_attributes[admin_collections_name].nil?) ? [] : 
-                            (self.user_attributes[admin_collections_name].is_a? Array) ? 
+    @user_collections ||= (self.user_attributes[admin_collections_name].nil?) ? [] :
+                            (self.user_attributes[admin_collections_name].is_a? Array) ?
                               self.user_attributes[admin_collections_name] : [self.user_attributes[admin_collections_name]]
   end
-  
+
   def admin_collections_name
     "umbra_admin_collections".to_sym
   end
