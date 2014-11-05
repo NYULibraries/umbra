e3ab0cdf5cbd8085c903427494a0daacf075ca75
B Alter <barnaby.alter@gmail.com>
2014-11-05 18:40:20 -0500

    Updated staging environemtn for rails 4

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,28 +1,27 @@
-Umbra::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+Rails.application.configure do
   config.action_controller.relative_url_root = '/umbra'
+  # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :yui
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  # Defaults to Rails.root.join("public/assets")
+  # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
@@ -30,10 +29,10 @@ Umbra::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  config.log_level = :debug
+  # config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
@@ -62,13 +61,4 @@ Umbra::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  # 
-  # Memcached
-  config.perform_caching = true
-  config.action_controller.perform_caching = true
-  config.cache_store = :dalli_store, 'localhost:11211'
 end
