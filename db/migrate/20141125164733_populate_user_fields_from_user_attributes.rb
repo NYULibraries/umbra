d16e38ff1962324b83e03498c5a62a80fc76e700
B Alter <barnaby.alter@gmail.com>
2014-12-04 15:18:04 -0500

    Serialize user_attributes in migration so we can interact with it and leave User model prisitine

--- db/migrate/20141125164733_populate_user_fields_from_user_attributes.rb
+++ db/migrate/20141125164733_populate_user_fields_from_user_attributes.rb
@@ -1,6 +1,7 @@
 class PopulateUserFieldsFromUserAttributes < ActiveRecord::Migration
   def up
     say_with_time "Migrating User Attributes" do
+      User.class_eval { serialize :user_attributes }
       User.all.each do |user|
         user.update_attribute :aleph_id, user.user_attributes[:nyuidn]
         user.update_attribute :status, user.user_attributes[:bor_status]
