b7f53dbf6767edef0e8988c0fd74d836f6724a9f
hab278 <hab278@nyu.edu>
2015-05-12 17:40:37 -0400

    Test the redirect and cookie checking

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -3,9 +3,26 @@ describe CatalogController do
 
   let!(:dss_record) { create(:dss_record) }
   let!(:vbl_record) { create(:vbl_record) }
-  before(:each) { @request.cookies["_check_passive_login"] = true }
+
+
+  describe "GET root" do
+    context 'when _check_passive_login cookie has been set' do
+      before { @request.cookies["_check_passive_login"] = true }
+      before {  get :index, collection: "vbl" }
+      subject { response }
+      it("should have a 200 status") { expect(subject.status).to be(200) }
+    end
+    context 'when _check_passive_login cookie has not been set' do
+      before {  get :index, collection: "vbl" }
+      subject { response }
+      it("should have a 302 status") { expect(subject.status).to be(302) }
+      it { should redirect_to("#{ENV['PASSIVE_LOGIN_URL']}?login_url=#{request.base_url}#{login_path}&return_uri=#{request.url}") }
+      it("should set _check_passive_login cookie") { expect(subject.cookies["_check_passive_login"]).to be_true }
+    end
+  end
 
   describe "GET /index" do
+    before(:each) { @request.cookies["_check_passive_login"] = true }
     it "should not get search results when searching an invalid term" do
       get :index, q: "danjkdw", search_field: "all_fields"
       expect(assigns_response.docs.size).to be 0
