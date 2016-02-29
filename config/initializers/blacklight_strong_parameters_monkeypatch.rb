7901aea34c4223d101fb0c88ff85c9df132aba61
B Alter <barnaby.alter@gmail.com>
2016-02-29 16:11:20 -0500

    Deleted defunct monkey patch because strong params were implemented a while ago, Fixes #8

--- config/initializers/blacklight_strong_parameters_monkeypatch.rb
+++
@@ -1,4 +0,0 @@
-# MonkeyPatch blacklight to allow non-strong parameters for Rails4
-# This is transitional need while we don't use strong parameters
-# Blacklight::Search.class_eval { attr_accessible :query_params }
-# Blacklight::Bookmark.class_eval { attr_accessible :id, :document_id, :document_type, :title }
